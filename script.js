4deb384386994ef94a4a23595d5359672112bf4c
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2016-05-08 21:51:41 +0530

    restructure

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+var classes = ['c0', 'c1', 'c11', 'c12', 'c13', 'c14', 'c15', 'c2', 'c211', 'c212', 'c213', 'c214', 'c215', 'c216', 'c221', 'c222', 'c223', 'c224', 'c225', 'c226', 'c227'];
+var texts = ['chessengines', 'minimax', 'alphabetapruning', 'neuralnetworks', 'gradientdescent', 'backpropagation', 'wordvectors'];
+var backgrounds = ['background2.jpeg', 'background3.jpeg', 'background4.jpeg', 'background5.jpg', 'background6.jpeg', 'background1.jpeg'];
+
+var backindex = 0;
+var lengths = [6, texts.length];
+
 var navigators = document.getElementsByClassName('navigator');
 var presenters = document.getElementsByClassName('presenter');
-var c0 = document.getElementsByClassName('c0');
-var c1 = document.getElementsByClassName('c1');
-var c11 = document.getElementsByClassName('c11');
-var c12 = document.getElementsByClassName('c12');
-var c2 = document.getElementsByClassName('c2');
-var c211 = document.getElementsByClassName('c211');
-var c212 = document.getElementsByClassName('c212');
-var c213 = document.getElementsByClassName('c213');
-var c214 = document.getElementsByClassName('c214');
-var c215 = document.getElementsByClassName('c215');
-var c216 = document.getElementsByClassName('c216');
-var c221 = document.getElementsByClassName('c221');
-var c222 = document.getElementsByClassName('c222');
-var c223 = document.getElementsByClassName('c223');
-var c224 = document.getElementsByClassName('c224');
-var c225 = document.getElementsByClassName('c225');
-var c226 = document.getElementsByClassName('c226');
-var c227 = document.getElementsByClassName('c227');
+
+for(var i = 0; i < classes.length; ++i)
+	window[classes[i]] = document.getElementsByClassName(classes[i]);
+
+function textfunction(texts, i) {
+	return function() {
+		document.getElementById(texts[i]).innerHTML = window['client' + i].responseText;
+	}
+}
+
+for(var i = 0; i < texts.length; ++i) {
+	window['client' + i] = new XMLHttpRequest();
+	window['client' + i].open('GET', 'texts/' + texts[i] + '.html');
+	window['client' + i].onreadystatechange = textfunction(texts, i);
+	window['client' + i].send();
+}
 
 function setDisplay(array, value) {
 	for(var i = 0; i < array.length; ++i)
@@ -27,37 +32,19 @@ function setDisplay(array, value) {
 function clicker() {
 	var superstate = localStorage.getItem('superstate');
 	var substate = localStorage.getItem('substate');
+	for(var i = 0; i < classes.length; ++i)
+		setDisplay(window[classes[i]], 'none');
 	if(substate === '0') {
 		for(var i = 0; i < navigators.length; ++i)
 			navigators[i].style.height = '100%';
 		for(var i = 0; i < presenters.length; ++i)
 			presenters[i].style.width = '0';
-		setDisplay(c2, 'none');
-		setDisplay(c211, 'none');
-		setDisplay(c212, 'none');
-		setDisplay(c213, 'none');
-		setDisplay(c214, 'none');
-		setDisplay(c215, 'none');
-		setDisplay(c216, 'none');
-		setDisplay(c221, 'none');
-		setDisplay(c222, 'none');
-		setDisplay(c223, 'none');
-		setDisplay(c224, 'none');
-		setDisplay(c225, 'none');
-		setDisplay(c226, 'none');
-		setDisplay(c227, 'none');
-		setDisplay(c1, 'none');
-		setDisplay(c11, 'none');
-		setDisplay(c12, 'none');
 		if(superstate === '0')
 			document.getElementById('subnavigation').style.height = '0';
 		else {
 			document.getElementById('subnavigation').style.height = '100%';
 			setDisplay(c1, 'block');
-			if(superstate === '1')
-				setDisplay(c11, 'block');
-			else if(superstate === '2')
-				setDisplay(c12, 'block');
+			setDisplay(window['c1' + superstate], 'block');
 		}
 		setDisplay(c0, 'block');
 	}
@@ -66,184 +53,56 @@ function clicker() {
 			navigators[i].style.height = '20%';
 		document.getElementById('imagesection').style.width = '69%';
 		document.getElementById('textsection').style.width = '30%';
-		setDisplay(c0, 'none');
-		setDisplay(c1, 'none');
-		setDisplay(c11, 'none');
-		setDisplay(c12, 'none');
-		setDisplay(c211, 'none');
-		setDisplay(c212, 'none');
-		setDisplay(c213, 'none');
-		setDisplay(c214, 'none');
-		setDisplay(c215, 'none');
-		setDisplay(c216, 'none');
-		setDisplay(c221, 'none');
-		setDisplay(c222, 'none');
-		setDisplay(c223, 'none');
-		setDisplay(c224, 'none');
-		setDisplay(c225, 'none');
-		setDisplay(c226, 'none');
-		setDisplay(c227, 'none');
-		if(superstate === '1') {
-			if(substate === '1')
-				setDisplay(c211, 'block');
-			else if(substate === '2')
-				setDisplay(c212, 'block');
-			else if(substate === '3')
-				setDisplay(c213, 'block');
-			else if(substate === '4')
-				setDisplay(c214, 'block');
-			else if(substate === '5')
-				setDisplay(c215, 'block');
-			else if(substate === '6')
-				setDisplay(c216, 'block');
-		}
-		else if(superstate === '2') {
-			if(substate === '1')
-				setDisplay(c221, 'block');
-			else if(substate === '2')
-				setDisplay(c222, 'block');
-			else if(substate === '3')
-				setDisplay(c223, 'block');
-			else if(substate === '4')
-				setDisplay(c224, 'block');
-			else if(substate === '5')
-				setDisplay(c225, 'block');
-			else if(substate === '6')
-				setDisplay(c226, 'block');
-			else if(substate === '7')
-				setDisplay(c227, 'block');
-		}
+		setDisplay(window['c2' + superstate + substate], 'block');
 		setDisplay(c2, 'block');
 	}
 }
 
-document.getElementById('namesection').onclick = function() {
-	localStorage.setItem('superstate', 0);
-	localStorage.setItem('substate', 0);
-	clicker();
+function statefunction(superstate, substate) {
+	return function() {
+		localStorage.setItem('superstate', superstate);
+		localStorage.setItem('substate', substate);
+		clicker();
+	}
 }
 
-document.getElementById('l1').onclick = function() {
-	localStorage.setItem('superstate', 1);
-	localStorage.setItem('substate', 0);
-	clicker();
-}
+document.getElementById('namesection').onclick = statefunction(0, 0);
+document.getElementById('l10').onclick = statefunction(1, 0);
+document.getElementById('l20').onclick = statefunction(2, 0);
+document.getElementById('l30').onclick = statefunction(3, 0);
+document.getElementById('l40').onclick = statefunction(4, 0);
+document.getElementById('l50').onclick = statefunction(5, 0);
 
-document.getElementById('l2').onclick = function() {
-	localStorage.setItem('superstate', 2);
-	localStorage.setItem('substate', 0);
-	clicker();
-}
-
-document.getElementById('l11').onclick = function() {
-	localStorage.setItem('superstate', 1);
-	localStorage.setItem('substate', 1);
-	clicker();
-}
-
-document.getElementById('l12').onclick = function() {
-	localStorage.setItem('superstate', 1);
-	localStorage.setItem('substate', 2);
-	clicker();
-}
-
-document.getElementById('l13').onclick = function() {
-	localStorage.setItem('superstate', 1);
-	localStorage.setItem('substate', 3);
-	clicker();
-}
-
-document.getElementById('l14').onclick = function() {
-	localStorage.setItem('superstate', 1);
-	localStorage.setItem('substate', 4);
-	clicker();
-}
-
-document.getElementById('l15').onclick = function() {
-	localStorage.setItem('superstate', 1);
-	localStorage.setItem('substate', 5);
-	clicker();
-}
-
-document.getElementById('l16').onclick = function() {
-	localStorage.setItem('superstate', 1);
-	localStorage.setItem('substate', 6);
-	clicker();
-}
-
-document.getElementById('l21').onclick = function() {
-	localStorage.setItem('superstate', 2);
-	localStorage.setItem('substate', 1);
-	clicker();
-}
-
-document.getElementById('l22').onclick = function() {
-	localStorage.setItem('superstate', 2);
-	localStorage.setItem('substate', 2);
-	clicker();
-}
-
-document.getElementById('l23').onclick = function() {
-	localStorage.setItem('superstate', 2);
-	localStorage.setItem('substate', 3);
-	clicker();
-}
-
-document.getElementById('l24').onclick = function() {
-	localStorage.setItem('superstate', 2);
-	localStorage.setItem('substate', 4);
-	clicker();
-}
-
-document.getElementById('l25').onclick = function() {
-	localStorage.setItem('superstate', 2);
-	localStorage.setItem('substate', 5);
-	clicker();
-}
-
-document.getElementById('l26').onclick = function() {
-	localStorage.setItem('superstate', 2);
-	localStorage.setItem('substate', 6);
-	clicker();
-}
-
-document.getElementById('l27').onclick = function() {
-	localStorage.setItem('superstate', 2);
-	localStorage.setItem('substate', 7);
-	clicker();
-}
+for(var superstate  = 0; superstate < lengths.length; ++superstate)
+	for(var substate = 0; substate < lengths[superstate]; ++substate)
+		document.getElementById('l' + String(superstate + 1) + String(substate + 1)).onclick = statefunction(superstate + 1, substate + 1);
 
 document.getElementById('upbutton').onclick = function() {
 	localStorage.setItem('substate', 0);
 	clicker();
 }
 
-var lengths = [6, 7];
-
 document.getElementById('leftbutton').onclick = function() {
-	substate = localStorage.getItem('substate');
-	if(parseInt(substate) === 1)
+	substate = parseInt(localStorage.getItem('substate'));
+	if(substate === 1)
 		localStorage.setItem('substate', lengths[parseInt(localStorage.getItem('superstate')) - 1]);
 	else
-		localStorage.setItem('substate', parseInt(substate) - 1);
+		localStorage.setItem('substate', substate - 1);
 	clicker();
 }
 
 document.getElementById('rightbutton').onclick = function() {
-	substate = localStorage.getItem('substate');
-	if(parseInt(substate) === lengths[parseInt(localStorage.getItem('superstate')) - 1])
+	substate = parseInt(localStorage.getItem('substate'));
+	if(substate === lengths[parseInt(localStorage.getItem('superstate')) - 1])
 		localStorage.setItem('substate', 1);
 	else
-		localStorage.setItem('substate', parseInt(substate) + 1);
+		localStorage.setItem('substate', substate + 1);
 	clicker();
 }
 
 localStorage.setItem('superstate', 0);
 localStorage.setItem('substate', 0);
 clicker();
-
-var backindex = 0;
-var backgrounds = ['background2.jpeg', 'background3.jpeg', 'background4.jpeg', 'background5.jpg', 'background6.jpeg', 'background1.jpeg'];
 
 setInterval(function() {
 	document.getElementById('back').setAttribute('src', 'images/' + backgrounds[backindex]);
